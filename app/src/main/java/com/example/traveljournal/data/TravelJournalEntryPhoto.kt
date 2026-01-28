1b2426176153684cd992fe04fc750a169e5d3637
Pavel Shvets <pavel.v.shvets@gmail.com>
2026-01-28 11:25:26 -0700

    added TravelJournalEntryPhoto class

---
+++ app/src/main/java/com/example/traveljournal/data/TravelJournalEntryPhoto.kt
@@ -0,0 +1,22 @@
+package com.example.traveljournal.data
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "travel_entries_photos",
+    foreignKeys = [
+        ForeignKey(
+            entity = TravelJourneyEntry::class,
+            parentColumns = ["id"],
+            childColumns = ["entryId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
+
+data class TravelJournalEntryPhoto (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val entryId: Int,
+    val photoUri: String
+    )
